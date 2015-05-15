d374eff2b421db770a7e2d1dca3d1a3fc02a56cb
Ed <e@s.com>
2015-05-15 15:27:24 +1200

    removed }

--- schema.js
+++ schema.js
@@ -19,7 +19,7 @@ client = new pg.Client(connectionString);
 client.connect();
 
 	query = client.query( 'CREATE TABLE users ( user_id SERIAL PRIMARY KEY, username varchar(30) NOT NULL, password bytea NOT NULL,first_name varchar(20), last_name(30), age smallint NOT NULL, difficulty smallint NOT NULL)'  );
-	query.on('end',function(err,result){ }  });	
+	query.on('end',function(err,result){ }  );	
 client.end();
 /*
 client = new pg.Client(connectionString);
