c3bb5dbaacf6e1d93eedd7e823cb71ddd23d991d
Ed <e@s.com>
2015-05-15 15:29:59 +1200

    varchar

--- schema.js
+++ schema.js
@@ -18,7 +18,7 @@ client.end();
 client = new pg.Client(connectionString);
 client.connect();
 
-	query = client.query( 'CREATE TABLE users ( user_id SERIAL PRIMARY KEY, username varchar(30) NOT NULL, password bytea NOT NULL,first_name varchar(20), last_name(30), age smallint NOT NULL, difficulty smallint NOT NULL)'  );
+	query = client.query( 'CREATE TABLE users ( user_id SERIAL PRIMARY KEY, username varchar(30) NOT NULL, password bytea NOT NULL,first_name varchar(20), last_name varchar(30), age smallint NOT NULL, difficulty smallint NOT NULL)'  );
 	query.on('end',function(err,result){ }  );	
 client.end();
 /*
