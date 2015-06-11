2bd413ba99db5d0de414451ce36226d7c1f9b23b
Aaron Haslett <aaron.haslett@gmail.com>
2015-06-12 01:55:43 +1200

    Altering schema and code to expand auth

--- index.js
+++ index.js
@@ -98,13 +98,13 @@ app.get('/', function(request, response) {
   });
 });
 
-app.get('/account', function(request,response){
+app.get('/account', getauth, function(request,response){
   if(!request.isAuthenticated()){
     response.redirect('/login');
     return;
   } 
   
-  response.send("ID: " + request.user.id + "\nName: " + request.user.displayName);
+  response.send("ID: " + request.user.user_id + "\nName: " + request.user.display_name);
 }); 
 
 app.get('/login', function(req, res){
@@ -115,13 +115,59 @@ app.get('/login', function(req, res){
 app.get('/auth/google',passport.authenticate('google', {scope: ['email']}));
 
 app.get('/authredir',
-  passport.authenticate('google', {failureRedirect: '/login'}),
+  passport.authenticate('google', {failureRedirect: '/login', session:false}),
   function(req,res){
-    everything = req.user.access_token + "\n\n";
-    for(key in req.user){everything += "\n" + key + ":: " + req.user[key];}
-    res.send('authenticated :) req contents:\n\n' + everything);
+    query = client.query('SELECT token FROM users WHERE user_id=$1',[req.user.id]);
+    token = null;
+    query.on('row', function(d){token = d.token;});
+    query.on('end', function(){
+      //If token is null, then no user was found.  Make a new entry.
+      //Otherwise, the token is either still extant or empty.  Either way, we make a new one and return it.
+      insertOrUpdate = null;
+      if (token==null){
+        insertOrUpdate = client.query('INSERT INTO users (user_id,display_name,difficulty,token) VALUES ' +
+                                                        '($1,     $2,          $3,        $4)',
+                                                     [req.user.id, req.user.displayName, 1, req.user.access_token]);
+      }
+      else{
+        insertOrUpdate = client.query('UPDATE users SET token=$1 WHERE user_id=$2',[req.user.access_token,req.user.user_id]);
+      }
+
+      insertOrUpdate.on('end', function(){
+        res.send({'access_token': req.user.access_token, 'name': req.user.displayName});
+      });
+    });
   }
 );
+
+function postauth(req,res,next){
+  return auth(req.body,req,res,next);
+}
+
+function getauth(req,res,next){
+  return auth(req.query,req,res,next);
+}
+
+function auth(params,req,res,next){
+  if(!params.hasOwnProperty('token')){
+    res.statusCode = 400;
+    res.send('Error 400: Token not provided');
+    return false;
+  }
+
+  user = null;
+  query = client.query('SELECT user_id,display_name,token,difficulty FROM users WHERE token=$1', [params.token]);
+  query.on('row', function(d){user=d;});
+  query.on('end', function(){
+    if(user==null){
+      res.statusCode = 401;
+      res.send("Token " + params.token + " does not exist");
+    }else{
+      req.user = user;
+      next()
+    }
+  });
+}
 
 /*OAuth toy code ends*/
 
--- schema.js
+++ schema.js
@@ -7,8 +7,8 @@ client = new pg.Client(connectionString);
 client.connect();
 
 client.query(
-    'CREATE TABLE users ( user_id SERIAL PRIMARY KEY, username varchar(30) NOT NULL, password bytea NOT NULL,first_name varchar(20), last_name varchar(30), age smallint NOT NULL, difficulty smallint NOT NULL);' +
-    'CREATE TYPE status AS ENUM(\'issued\',\'completed\');' +	
+    'CREATE TABLE users ( user_id SERIAL PRIMARY KEY, display_name varchar(30), difficulty smallint NOT NULL, token varchar(36));' +
+    'CREATE TYPE status AS ENUM(\'issued\',\'completed\');' +
     'CREATE TYPE part AS ENUM(\'n\',\'v\',\'pn\',\'adj\',\'adv\',\'c\',\'pr\',\'i\');' +
     'CREATE TABLE friends (user_id integer references users(user_id), friend_id integer references users(user_id),PRIMARY KEY(user_id,friend_id));' +
     'CREATE TABLE parents(user_id integer references users(user_id) NOT NULL, parent_gmail varchar(30) NOT NULL);' +
